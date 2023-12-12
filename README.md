fb48d822b0a0b995cbad4a0494b2bd584f0a98b9
FireStreaker2 <michaelccc2019@outlook.com>
2023-12-11 18:38:05 -0800

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# hentairead-js
- Node wrapper for hentairead.com
+![hentairead-js](https://socialify.git.ci/FireStreaker2/hentairead-js/image?description=1&font=Raleway&forks=1&issues=1&language=1&name=1&owner=1&pattern=Solid&pulls=1&stargazers=1&theme=Dark)
+
+# About
+hentairead-js is a simple node package to scrape https://hentairead.com. It utilizes [node-fetch](https://www.npmjs.com/package/node-fetch) and [cheerio](https://www.npmjs.com/package/cheerio). It was made because many nhentai packages stopped working due to cloudflare issues.
+
+# Features
+* Scrape pages quickly and easily
+* Fully asynchronous
+* Supports most actions reading-wise
+
+# Installation
+```bash
+$ npm i hentairead-js
+```
+# Usage
+| Method      | Description                                                  |
+|-------------|--------------------------------------------------------------|
+| searchPage  | Scrape a page and return all the doujins listed on that page |
+| search      | Search for a specific doujin with many parameters            |
+| exists      | Check whether a doujin exists or not                         |
+| top         | Get the top doujins based off of the provided range          |
+| getInfo     | Get the info of a specific doujin                            |
+| getPages    | Get all the pages of a specific doujin                       |
+| searchIndex | Search through a specific index with a specific query        |
+| getHome     | Get the doujins displayed on the home page                   |
+| getAll      | Get a list of all doujins                                    |
+| getRandom   | Get a random doujin                                          |
+
+# Examples
+```js
+// index.js
+const HentaiRead = require("hentairead-js");
+
+(async () => {
+  // search for the first page of all doujins that have the word "neko"
+  console.log(await HentaiRead.search({ s: "neko", page: 1 }));
+
+  // get pages for a specific doujin
+  console.log(await HentaiRead.getPages("boku-no-risou-no-isekai-seikatsu-4-decensored"));
+})();
+```
+
+# Troubleshooting
+| Problem                                                      | Answer                                                                                                                                                                                                                                                                                                                                        |
+|--------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| What parameters does the ``search`` method accept?           | The search method accepts anything as a parameter, because of all the possible variations of searches. While it can't check if something is valid or not, it will throw an error if a ``404`` is found. For a list of all accepted parameters, you can look at the query parameters found [here](https://hentairead.com/?s) (click advanced). |
+| Why do I get a timeout error? / Why does it keep on hanging? | Try checking your internet connection to make sure it's stable.                                                                                                                                                                                                                                                                               |
+| Does this support synchronous functions?                     | No, everything is fully asynchronous                                                                                                                                                                                                                                                                                                          |
+
+# Contributing
+If you would like to contribute, you can <a href="https://github.com/FireStreaker2/hentairead-js/fork">fork the repo</a> and <a href="https://github.com/FireStreaker2/hentairead-js/compare">make a PR</a>, or contact me via email @ ``suggestions@firestreaker2.gq``
+
+# License
+[MIT](https://github.com/FireStreaker2/hentairead-js/blob/main/LICENSE)
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 
+/**
+ * HentaiRead class
+ */
 class HentaiRead {
+	/**
+	 * Scrape a page and return all the doujins listed on that page
+	 * @param {string} url - URL to scrape
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async searchPage(url) {
 		if (typeof url !== "string") throw new Error("URL must be a string");
 
@@ -63,6 +74,14 @@ class HentaiRead {
 		}
 	}
 
+	/**
+	 * Search for a specific doujin with many parameters
+	 * @param {object} options - An object representing query strings to search with
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async search(options) {
 		return await this.searchPage(
 			`https://hentairead.com/page/${options.page}/?${new URLSearchParams(
@@ -71,6 +90,14 @@ class HentaiRead {
 		);
 	}
 
+	/**
+	 * Check whether a doujin exists or not
+	 * @param {string} query - The search query
+	 * @returns {Promise} - Promise representing a boolean of whether the doujin exists or not
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async exists(query) {
 		if (typeof query !== "string") throw new Error("Query must be a string");
 
@@ -80,6 +107,15 @@ class HentaiRead {
 		return false;
 	}
 
+	/**
+	 * Get the top doujins based off of the provided range
+	 * @param {string} range - How far to look
+	 * @param {number} page - Page number to look at
+	 * @returns {Promise} A promise that contains an array of results
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async top(range, page = 1) {
 		const allowed = ["day", "week", "month", "year", "all"];
 
@@ -91,6 +127,14 @@ class HentaiRead {
 		);
 	}
 
+	/**
+	 * Get the info of a specific doujin
+	 * @param {string} doujin - The doujin to search for
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async getInfo(doujin) {
 		if (typeof doujin !== "string")
 			throw new Error("Doujin name must be a string");
@@ -174,6 +218,14 @@ class HentaiRead {
 		}
 	}
 
+	/**
+	 * Get all the pages of a specific doujin
+	 * @param {string} doujin
+	 * @returns {Promise} A promise that contains an array with the corresponding images
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async getPages(doujin) {
 		if (typeof doujin !== "string")
 			throw new Error("Doujin name must be a string");
@@ -203,6 +255,18 @@ class HentaiRead {
 		}
 	}
 
+	/**
+	 * Search through a specific index with a specific query
+	 * @param {string} index - The index to search through
+	 * @param {string} query - The query to search with
+	 * @param {number} page - Page number to look at
+	 * @param {string} sort - How to search the results
+	 * @param {string} order - Whether to display the results in ascending or descending order
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async searchIndex(
 		index,
 		query,
@@ -230,16 +294,41 @@ class HentaiRead {
 		);
 	}
 
+	/**
+	 * Get the doujins displayed on the home page
+	 * @param {number} page - Page number to look at
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async getHome(page = 1) {
 		return this.searchPage(`https://hentairead.com/page/${page}`);
 	}
 
+	/**
+	 * Get a list of all doujins
+	 * @param {number} page - Page number to look at
+	 * @param {string} sort - How to search the results
+	 * @param {string} order - Whether to display the results in ascending or descending order
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async getAll(page = 1, sort = "new", order = "desc") {
 		return this.searchPage(
 			`https://hentairead.com/hentai/page/${page}/?m_orderby=${sort}&m_order=${order}`
 		);
 	}
 
+	/**
+	 * Get a random doujin
+	 * @returns {Promise} A promise that contains an object with the corresponding info
+	 * @throws {error} If there is an error while searching
+	 * @static
+	 * @async
+	 */
 	static async getRandom() {
 		try {
 			const response = await fetch("https://hentairead.com/?random_manga=1");
